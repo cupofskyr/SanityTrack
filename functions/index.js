3f221f569840aa6cc99cd97d589d89cdf787b402
UHD <Unnarh@gmail.com>
2025-07-03 15:22:00 +0000

    okay i just restored a older version Rolling back to version 14ea9819, w

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,22 @@
 
-// =================================================================
-// PART 1 & 2: BACKEND & SYSTEM GLUE
-// =================================================================
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const inventoryFunctions = require("./inventory");
-
+const next = require("next");
 
 admin.initializeApp();
 const db = admin.firestore();
+
+// --- Next.js Server ---
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, conf: { distDir: "../.next" } });
+const handle = app.getRequestHandler();
+
+exports.nextServer = functions.https.onRequest((req, res) => {
+  return app.prepare().then(() => handle(req, res));
+});
+// --- End Next.js server ---
+
 
 // Export inventory-related functions
 exports.handlePOSSale = inventoryFunctions.handlePOSSale;
